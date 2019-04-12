cc3827ed659b9cfbd24a895b5b8e4fc30dccb829
JamesC <nfftfs2024@gmail.com>
2019-04-12 23:59:40 +1000

    Add project files.

---
+++ MedImgDBMS/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MedImgDBMS.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
