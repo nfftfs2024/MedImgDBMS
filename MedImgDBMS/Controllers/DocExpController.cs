eff0874fb4ddf3708c73e9ffd3ca8a3864a083ac
JamesC <nfftfs2024@gmail.com>
2019-04-23 23:57:42 +1000

    Clean up DocExp controller and views
    Clean up DocExp controller and views

--- MedImgDBMS/Controllers/DocExpController.cs
+++ MedImgDBMS/Controllers/DocExpController.cs
@@ -15,7 +15,7 @@ namespace MedImgDBMS.Controllers
     {
         private pjmedimgdbEntities db = new pjmedimgdbEntities();
 
-        // GET: Images
+        // GET: DocExp
         public ActionResult Index(string preColumn, string searchString, string statusString)
         {
             int userID = Convert.ToInt32(Session["UserID"] != null ? Session["UserID"].ToString() : "0");   // Convert session user id to integer for comparison and prevent from NULL
@@ -251,121 +251,6 @@ namespace MedImgDBMS.Controllers
                 }
             }
             return RedirectToAction("ExpImageView", id);    // Reload page
-        }
-
-        // GET: Images/Details/5
-        public ActionResult Details(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            image image = db.images.Find(id);
-            if (image == null)
-            {
-                return HttpNotFound();
-            }
-            return View(image);
-        }
-
-        // GET: Images/Create
-        public ActionResult Create()
-        {
-            ViewBag.ImgPatID = new SelectList(db.patients, "PatID", "PatLName");
-            ViewBag.StatusID = new SelectList(db.imagestatus, "ImgStatID", "StatusName");
-            ViewBag.ImgCreator = new SelectList(db.users, "UserID", "UserLName");
-            ViewBag.ImgDocID = new SelectList(db.users, "UserID", "UserLName");
-            ViewBag.ImgExpID = new SelectList(db.users, "UserID", "UserLName");
-            return View();
-        }
-
-        // POST: Images/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ImgID,ImgPath,ImgName,ImgCreateTime,ImgCreator,ImgExpID,ImgDocID,ImgPatID,StatusID")] image image)
-        {
-            if (ModelState.IsValid)
-            {
-                db.images.Add(image);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.ImgPatID = new SelectList(db.patients, "PatID", "PatLName", image.ImgPatID);
-            ViewBag.StatusID = new SelectList(db.imagestatus, "ImgStatID", "StatusName", image.ImgStatus);
-            ViewBag.ImgCreator = new SelectList(db.users, "UserID", "UserLName", image.ImgCreator);
-            ViewBag.ImgDocID = new SelectList(db.users, "UserID", "UserLName", image.ImgDocID);
-            ViewBag.ImgExpID = new SelectList(db.users, "UserID", "UserLName", image.ImgExpID);
-            return View(image);
-        }
-
-        // GET: Images/Edit/5
-        public ActionResult Edit(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            image image = db.images.Find(id);
-            if (image == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ImgPatID = new SelectList(db.patients, "PatID", "PatLName", image.ImgPatID);
-            ViewBag.StatusID = new SelectList(db.imagestatus, "ImgStatID", "StatusName", image.ImgStatus);
-            ViewBag.ImgCreator = new SelectList(db.users, "UserID", "UserLName", image.ImgCreator);
-            ViewBag.ImgDocID = new SelectList(db.users, "UserID", "UserLName", image.ImgDocID);
-            ViewBag.ImgExpID = new SelectList(db.users, "UserID", "UserLName", image.ImgExpID);
-            return View(image);
-        }
-
-        // POST: Images/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ImgID,ImgPath,ImgName,ImgCreateTime,ImgCreator,ImgExpID,ImgDocID,ImgPatID,StatusID")] image image)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(image).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ImgPatID = new SelectList(db.patients, "PatID", "PatLName", image.ImgPatID);
-            ViewBag.StatusID = new SelectList(db.imagestatus, "ImgStatID", "StatusName", image.ImgStatus);
-            ViewBag.ImgCreator = new SelectList(db.users, "UserID", "UserLName", image.ImgCreator);
-            ViewBag.ImgDocID = new SelectList(db.users, "UserID", "UserLName", image.ImgDocID);
-            ViewBag.ImgExpID = new SelectList(db.users, "UserID", "UserLName", image.ImgExpID);
-            return View(image);
-        }
-
-        // GET: Images/Delete/5
-        public ActionResult Delete(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            image image = db.images.Find(id);
-            if (image == null)
-            {
-                return HttpNotFound();
-            }
-            return View(image);
-        }
-
-        // POST: Images/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(long id)
-        {
-            image image = db.images.Find(id);
-            db.images.Remove(image);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
